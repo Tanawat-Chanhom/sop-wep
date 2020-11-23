b043e58527757605d471eb2871c993df62573342
Tanawat_Chanhom <tae_chanhom@hotmail.com>
2020-11-24 00:05:57 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/home";
+import Haeder from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Haeder></Haeder>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
